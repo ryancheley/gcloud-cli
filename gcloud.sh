bd3e314964364f7ffbc26f8b32b04bb0cbe2875d
Ryan Cheley <9857779+ryancheley@users.noreply.github.com>
2022-12-20 20:43:20 -0800

    different setp 1

--- gcloud.sh
+++ gcloud.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
