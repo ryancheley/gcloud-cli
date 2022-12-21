2b740131e488f36bf3829693f8829bc1011a2610
Ryan Cheley <9857779+ryancheley@users.noreply.github.com>
2022-12-20 20:49:32 -0800

    Let's include a new dependency

--- gcloud.sh
+++ gcloud.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
