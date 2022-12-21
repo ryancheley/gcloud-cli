fe5124046289fdafcaca54e527f2b82c20e409d7
Ryan Cheley <9857779+ryancheley@users.noreply.github.com>
2022-12-20 21:13:52 -0800

    one more try

--- gcloud.sh
+++ gcloud.sh
@@ -2,3 +2,4 @@
 sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install google-cloud-cli
