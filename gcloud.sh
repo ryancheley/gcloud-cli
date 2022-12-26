ae1fb995c1ca51e3daaaddee4c61722863392c13
Ryan Cheley <9857779+ryancheley@users.noreply.github.com>
2022-12-26 14:23:09 -0800

    Update gcloud.sh
    Update to work on Ubuntu 22.04

--- gcloud.sh
+++ gcloud.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo apt-get install apt-transport-https ca-certificates gnupg -y
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat /etc/apt/sources.list.d/google-cloud-sdk.list
-# sudo apt-get update && sudo apt-get install google-cloud-cli
+apt update && apt upgrade
+apt install curl
+apt-get install apt-transport-https ca-certificates gnupg -y
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt update && apt install google-cloud-cli
