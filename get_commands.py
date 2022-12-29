fc258438892e39bee49d021ab22608b8904eac83
Ryan Cheley <rcheley@gmail.com>
2022-12-28 17:11:39 -0800

    update to exclude non-command

--- get_commands.py
+++ get_commands.py
@@ -6,6 +6,7 @@ def get_commands():
         lines[i] = lines[i].strip().split("\t")[0]    
     lines = [i for i in lines[1:] if i]
     commands = [lines[index] for index in range(2, len(lines), 2)]    
+    commands = [i for i in commands if i != 'COMMANDS']
     for command in commands:
         file_name = f'gcloud/{command}.txt'
         cmd = f'gcloud {command} --help > {file_name}'
